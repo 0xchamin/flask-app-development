1d1237af6e9315d6b4fb04f42696dd12513d0163
0xchamin <chmk90@gmail.com>
2018-05-06 20:39:40 +0800

    template and route created for register.html page

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    artcles = [
+        {
+            'id' : 1,
+            'title' : 'Article One',
+            'body' : '',
+            'author' : 'Chamin Nalinda',
+            'create_date' : '04-25-2018'
+        },
+        {
+            'id' : 2,
+            'title' : 'Article Two',
+            'body' : '',
+            'author' : 'Jayani Jayasuriya',
+            'create_date' : '04-26-2018'
+        },
+        {
+            'id' : 3,
+            'title' : 'Article Three',
+            'body' : '',
+            'author' : 'Harini Hapuarachchi',
+            'create_date' : '04-27-2018'
+        },
+    ]
+    return artcles
